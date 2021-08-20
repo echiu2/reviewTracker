ae6a4739bcbfb10e1a5bcfee0c459524280a9fc5
Edwin Chiu <echiu2@buffalo.edu>
2021-08-20 01:03:06 -0400

    changing url parameters

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
 from flask import Flask, jsonify, request
-import requests
-import json
-from services import get_page, get_reviews
+from threading import Thread
+from queue import Queue
+from services import get_page, get_reviews, url_threading
+from time import time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def search():
+# An example of a working url is http://127.0.0.1:5000/first-midwest-bank/52903183
+@app.route('/<lender_name>/<int:lender_id>')
+def search(lender_name, lender_id):
+    lender_name = lender_name
+    lender_id = lender_id
+    try:
+        # Create the url that we are making the request for
+        root_url = 'https://www.lendingtree.com/reviews/personal/' + lender_name + '/' + str(lender_id) + '/'
+        print(root_url)
+        # Parsing root pages html with beautiful soup
+        page = get_page(root_url)
 
-    # If request is a POST request
-    if request.method == 'POST':
-        root = str(request.form.get('root'))
-        if root != '':
-            page_list = []
-            curr_page = 1
+        #Check for invalid urls
+        if page == "Url is invalid or does not exist!":
+            return "Invalid url: Please try again", 400
 
-        page = get_page(root)
-        # max_page = int(page.find('a', {'class': 'pageNum'}).text)
+        max_pages = int(page.find('a', {'class': 'pageNum'}).text)
+        urls = []
 
-        while curr_page <= 10:
-            url = root + '?pid=' + str(curr_page)
-            page_id = get_page(url)
-            review_url = {
-                'id': str(curr_page),
-                'reviews': get_reviews(page_id),
-            }
-            page_list.append(review_url) 
-            curr_page += 1
+        #Create url for root url existing paginations
+        for i in range(1, max_pages+1):
+            url = root_url + '?pid=' + str(i)
+            urls.append(get_page(url))
 
-        return jsonify(page_list)
+        # Executing threading for multiple pages reviews
+        res = url_threading(urls)
 
-    # otherwise handle the GET request
-    return '''
-           <form method="POST">
-               <div><label>Please input the Lender's url: <input type="text" name="root"></label><input type="submit" value="Submit"></div>
-           </form>'''
+        return jsonify(res), 200
+
+    except:
+        return "Invalid URL Parameters"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- services.py
+++ services.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
-import cchardet
-import lxml
-import time
-import json
+from queue import Queue
+from threading import Thread
 
 session = requests.Session()
 
@@ -12,37 +10,51 @@ def get_page(url):
     try:
         response = session.get(url)
         page = BeautifulSoup(response.text, 'lxml')
-        return page.select('.reviewTitle')
+        return page
     except:
         return "Url is invalid or does not exist!"
 
+# parsing the url html content to get all the reviews
+def get_reviews(page, queue):
+    main_reviews = page.find_all('div', {'class': 'col-xs-12 mainReviews'})
+    hidden_reviews = page.find_all('div', {'class': 'col-xs-12 mainReviews hiddenReviews'})
+    all_reviews = main_reviews + hidden_reviews
+    reviews = []
 
-for i in range(499):
-    url = "https://www.lendingtree.com/reviews/personal/onemain-financial-inc/291320?pid={}".format(str(i))
-    print(get_page(url))
-
-# # parsing the url html content to get all the reviews
-# def get_reviews(page):
-#     main_reviews = page.find_all('div', {'class': 'col-xs-12 mainReviews'})
-#     hidden_reviews = page.find_all('div', {'class': 'col-xs-12 mainReviews hiddenReviews'})
-#     all_reviews = main_reviews + hidden_reviews
-#     print(all_reviews)
-#     reviews = []
-
-#     for review in all_reviews:
-#         consumer_info = review.find('p',{'class': 'consumerName'}).text.strip().replace('                                                                       ','').split(' ')
-#         review_content ={
-#         'lender': page.title.text.replace(' – Personal Loan Company Reviews | LendingTree',''),
-#         'title' : review.find('p',{'class': 'reviewTitle'}).text.strip(),
-#         'content' : review.find('p',{'class': 'reviewText'}).text.strip(),
-#         'author' : consumer_info[0],
-#         'location': ' '.join(consumer_info[2:]),
-#         'rating' : float(review.find('div',{'class': 'numRec'}).text.replace(' of 5)stars','').replace('(','').strip()),
-#         'date' : review.find('p',{'class': 'consumerReviewDate'}).text.replace('Reviewed in ','').strip(),
-#         }
-#         reviews.append(review_content)
+    for review in all_reviews:
+        consumer_info = review.find('p',{'class': 'consumerName'}).text.strip().replace('                                                                       ','').split(' ')
+        review_content ={
+        'lender': page.title.text.replace(' – Personal Loan Company Reviews | LendingTree',''),
+        'title' : review.find('p',{'class': 'reviewTitle'}).text.strip(),
+        'content' : review.find('p',{'class': 'reviewText'}).text.strip(),
+        'author' : consumer_info[0],
+        'location': ' '.join(consumer_info[2:]),
+        'rating' : float(review.find('div',{'class': 'numRec'}).text.replace(' of 5)stars','').replace('(','').strip()),
+        'date' : review.find('p',{'class': 'consumerReviewDate'}).text.replace('Reviewed in ','').strip(),
+        }
+        reviews.append(review_content)
     
-#     return reviews
+    queue.put(reviews)
+
+# Threading function to get all content in the page
+def url_threading(urls):
+    all_threads = []
+    all_results = []
+    my_queue = Queue()
+
+    for url in urls:
+        t = Thread(target=get_reviews, args=(url, my_queue))
+        t.start()
+        all_threads.append(t)
+
+    while len(all_results) < len(urls):
+        data = my_queue.get()
+        all_results.append(data)
+
+    res = [item for item in all_results]
+
+    return res
+
 
 
     
